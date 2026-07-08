69d28488e16545b11b77959aac33d5c224edb315
galaxiamiauu-droid <galaxia.miauu@gmail.com>
2026-07-08 03:08:24 -0500

    Add posts management functionality

---
+++ posts.js
@@ -0,0 +1,24 @@
+const posts = [];
+
+function addPost(date, title, url) {
+    posts.push({
+        date,
+        title,
+        url
+    });
+}
+
+
+// 💜 Tus publicaciones van aquí
+
+addPost(
+    "2026-07-05",
+    "Hola.",
+    "https://admeliorali.blogspot.com/2026/07/hola.html"
+);
+
+addPost(
+    "2026-07-07",
+    "Hellou HeLLOU",
+    "https://admeliorali.blogspot.com/2026/07/hola-que-tal-como-estas-jsjs.html"
+);
